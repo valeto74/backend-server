75710f915e5c2cb5084313698f1c4c99e35912e6
Jose Eladio Valenzuela <jvalenzuela@bal.com.mx>
2018-06-07 14:14:32 -0500

    Sección 12 terminanda - Backend listo para produccion

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+
+// Google
+module.exports.CLIENT_ID = '442737206823-dilej5tevnrv61sovd7bocf5qeafmjs3.apps.googleusercontent.com';
+// module.exports.SECRET = '6cWrrFy8KchLLrYmg70esp7t';
